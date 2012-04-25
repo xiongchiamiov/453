5fc761e7f7d4487de4e0dcaa102bfcfab1a46dc0
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-04-24 23:51:10 -0700

    tabs -> spaces

--- projects/02/lwp.c
+++ projects/02/lwp.c
@@ -35,57 +35,57 @@ int lwp_running;                       /* the index of the currently running LWP
  *         -1 if more than LWP_PROC_LIMIT threads already exist
  */
 int new_lwp(lwpfun function, void * argument, size_t stacksize) {
-	size_t stackSizeInBytes = stacksize * sizeof(int);
-	unsigned long * stackPointer;
-	unsigned long * basePointer;
-	
-	/* For simplicity's sake, we limit ourselves to a set number of LWPs. */
-	if (lwp_procs == LWP_PROC_LIMIT) {
-		return -1;
-	}
-	/* lwp_running is 0-based, so set it *before* incrementing lwp_procs. */
-	lwp_running = lwp_procs;
-	lwp_procs++;
-	
-	lwp_ptable[lwp_running].pid = nextPid++;
-	lwp_ptable[lwp_running].stack = malloc(stackSizeInBytes);
-	lwp_ptable[lwp_running].stacksize = stacksize;
-	
-	/* Build up our stack in preparation for lwp_yield. */
-	/* I am constantly confused about what the stack is supposed to look like.
-	 * In particular, vertical representations don't help at all, since I view
-	 * memory as going from left to right.  This is the current assumption:
-	 *
-	 * [ s t u f f ]    #######[ space for more stuff ]
-	 *              |_________| memory available for the stack to use
-	 *                  ^ stack pointer (top of stack)
-	 *                         ^ memory available to the process
-	 */
-	stackPointer = lwp_ptable[lwp_running].stack + stackSizeInBytes;
-	basePointer = stackPointer;
-	*stackPointer = (unsigned long)argument; /* Arg 0 */
-	stackPointer--;
-	*stackPointer = (unsigned long)lwp_exit;
-	stackPointer--;
-	*stackPointer = (unsigned long)function; /* Return Address */
-	stackPointer--;
-	/**stackPointer = (unsigned long)basePointer;*/
-	*stackPointer = 0xDEADBEEF;
-	basePointer = stackPointer;
-	/* Space for the 7 registers SAVE_STATE saves. */
-	stackPointer -= 7;
-	*stackPointer = (unsigned long)(basePointer);
-	
-	lwp_ptable[lwp_running].sp = stackPointer;
-	
-	return lwp_running;
+    size_t stackSizeInBytes = stacksize * sizeof(int);
+    unsigned long * stackPointer;
+    unsigned long * basePointer;
+    
+    /* For simplicity's sake, we limit ourselves to a set number of LWPs. */
+    if (lwp_procs == LWP_PROC_LIMIT) {
+        return -1;
+    }
+    /* lwp_running is 0-based, so set it *before* incrementing lwp_procs. */
+    lwp_running = lwp_procs;
+    lwp_procs++;
+    
+    lwp_ptable[lwp_running].pid = nextPid++;
+    lwp_ptable[lwp_running].stack = malloc(stackSizeInBytes);
+    lwp_ptable[lwp_running].stacksize = stacksize;
+    
+    /* Build up our stack in preparation for lwp_yield. */
+    /* I am constantly confused about what the stack is supposed to look like.
+     * In particular, vertical representations don't help at all, since I view
+     * memory as going from left to right.  This is the current assumption:
+     *
+     * [ s t u f f ]    #######[ space for more stuff ]
+     *              |_________| memory available for the stack to use
+     *                  ^ stack pointer (top of stack)
+     *                         ^ memory available to the process
+     */
+    stackPointer = lwp_ptable[lwp_running].stack + stackSizeInBytes;
+    basePointer = stackPointer;
+    *stackPointer = (unsigned long)argument; /* Arg 0 */
+    stackPointer--;
+    *stackPointer = (unsigned long)lwp_exit;
+    stackPointer--;
+    *stackPointer = (unsigned long)function; /* Return Address */
+    stackPointer--;
+    /**stackPointer = (unsigned long)basePointer;*/
+    *stackPointer = 0xDEADBEEF;
+    basePointer = stackPointer;
+    /* Space for the 7 registers SAVE_STATE saves. */
+    stackPointer -= 7;
+    *stackPointer = (unsigned long)(basePointer);
+    
+    lwp_ptable[lwp_running].sp = stackPointer;
+    
+    return lwp_running;
 }
 
 /**
  * @return the pid of the calling LWP.  Undefined if not called by a LWP.
  */
 int lwp_getpid() {
-	return lwp_ptable[lwp_running].pid;
+    return lwp_ptable[lwp_running].pid;
 }
 
 /**
@@ -94,27 +94,27 @@ int lwp_getpid() {
  * context, and returns.
  */
 void lwp_yield() {
-	/* Save context for later. */
-	SAVE_STATE();
-	
-	/* Save stack pointer for later. */
-	GetSP(lwp_ptable[lwp_running].sp);
-	
-	/* Pick a thread to run. */
-	if (gScheduler == NULL) {
-		/* Round-robin. */
-		lwp_running++;
-		/* Go back to the start of the list if we've reached the end. */
-		if (lwp_running == lwp_procs) {
-			lwp_running = 0;
-		}
-	} else {
-		lwp_running = gScheduler();
-	}
-	SetSP(lwp_ptable[lwp_running].sp);
-	
-	/* And run it. */
-	RESTORE_STATE();
+    /* Save context for later. */
+    SAVE_STATE();
+    
+    /* Save stack pointer for later. */
+    GetSP(lwp_ptable[lwp_running].sp);
+    
+    /* Pick a thread to run. */
+    if (gScheduler == NULL) {
+        /* Round-robin. */
+        lwp_running++;
+        /* Go back to the start of the list if we've reached the end. */
+        if (lwp_running == lwp_procs) {
+            lwp_running = 0;
+        }
+    } else {
+        lwp_running = gScheduler();
+    }
+    SetSP(lwp_ptable[lwp_running].sp);
+    
+    /* And run it. */
+    RESTORE_STATE();
 }
 
 /**
@@ -122,34 +122,34 @@ void lwp_yield() {
  * in the process table.  If there are no other threads, calls `lwp_stop()`.
  */
 void lwp_exit() {
-	int i;
-	
-	/* Return thread's stack to system. */
-	free(lwp_ptable[lwp_running].stack);
-	
-	/* Shift the process table. */
-	for (i=lwp_running+1; i<lwp_procs; i++) {
-		lwp_ptable[i-1] = lwp_ptable[i];
-	}
-	
-	/* Stop if we don't have any more threads. */
-	lwp_procs--;
-	if (lwp_procs == 0) {
-		lwp_stop();
-	} else {
-		/* Pick a thread to run. */
-		if (gScheduler == NULL) {
-			/* Round-robin. */
-			/* Let's start at the very beginning, a very good place to start. */
-			lwp_running = 0;
-		} else {
-			lwp_running = gScheduler();
-		}
-		SetSP(lwp_ptable[lwp_running].sp);
-		
-		/* And run it. */
-		RESTORE_STATE();
-	}
+    int i;
+    
+    /* Return thread's stack to system. */
+    free(lwp_ptable[lwp_running].stack);
+    
+    /* Shift the process table. */
+    for (i=lwp_running+1; i<lwp_procs; i++) {
+        lwp_ptable[i-1] = lwp_ptable[i];
+    }
+    
+    /* Stop if we don't have any more threads. */
+    lwp_procs--;
+    if (lwp_procs == 0) {
+        lwp_stop();
+    } else {
+        /* Pick a thread to run. */
+        if (gScheduler == NULL) {
+            /* Round-robin. */
+            /* Let's start at the very beginning, a very good place to start. */
+            lwp_running = 0;
+        } else {
+            lwp_running = gScheduler();
+        }
+        SetSP(lwp_ptable[lwp_running].sp);
+        
+        /* And run it. */
+        RESTORE_STATE();
+    }
 }
 
 /**
@@ -158,28 +158,28 @@ void lwp_exit() {
  * no LWPs, returns immediately.
  */
 void lwp_start() {
-	if (lwp_procs == 0) {
-		return;
-	}
-	
-	/* Save context for later. */
-	SAVE_STATE();
-	
-	/* Save stack pointer for later. */
-	GetSP(gStackPointer);
-	
-	/* Pick a thread to run. */
-	if (gScheduler == NULL) {
-		/* Round-robin. */
-		/* Let's start at the very beginning, a very good place to start. */
-		lwp_running = 0;
-	} else {
-		lwp_running = gScheduler();
-	}
-	SetSP(lwp_ptable[lwp_running].sp);
-	
-	/* And run it. */
-	RESTORE_STATE();
+    if (lwp_procs == 0) {
+        return;
+    }
+    
+    /* Save context for later. */
+    SAVE_STATE();
+    
+    /* Save stack pointer for later. */
+    GetSP(gStackPointer);
+    
+    /* Pick a thread to run. */
+    if (gScheduler == NULL) {
+        /* Round-robin. */
+        /* Let's start at the very beginning, a very good place to start. */
+        lwp_running = 0;
+    } else {
+        lwp_running = gScheduler();
+    }
+    SetSP(lwp_ptable[lwp_running].sp);
+    
+    /* And run it. */
+    RESTORE_STATE();
 }
 
 /**
@@ -189,14 +189,14 @@ void lwp_start() {
  * `lwp_start()`.
  */
 void lwp_stop() {
-	/* Save context for later. */
-	SAVE_STATE();
-	
-	/* Reset stack pointer to original context. */
-	SetSP(gStackPointer);
-	
-	/* Continue on our way. */
-	RESTORE_STATE();
+    /* Save context for later. */
+    SAVE_STATE();
+    
+    /* Reset stack pointer to original context. */
+    SetSP(gStackPointer);
+    
+    /* Continue on our way. */
+    RESTORE_STATE();
 }
 
 /**
@@ -208,6 +208,6 @@ void lwp_stop() {
  * round-robin scheduling.
  */
 void lwp_set_scheduler(schedfun scheduler) {
-	gScheduler = scheduler;
+    gScheduler = scheduler;
 }
 
