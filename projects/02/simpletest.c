afacd1d7676fe15538191c044e4256c34ec538ef
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-04-24 21:28:50 -0700

    update simpletest to find next failing case

--- projects/02/simpletest.c
+++ projects/02/simpletest.c
@@ -2,21 +2,42 @@
 
 #include <stdio.h>
 
+unsigned long * sp;
+unsigned long * bp;
+
 static void foo(void *);
+static void foo2(void *);
 
 int main(int argc, char *argv[]){
-    /* Should return immediately. */
-    /* lwp_start(); */
+    /* Returns immediately. */
+    lwp_start();
 
+    /* Prints "Hallo thar!" once and exits. */
     new_lwp(foo, (void *)212, 256);
+    lwp_start();
 
+    printf("---------------------------------\n");
+    
+    /* Returns immediately. */
+    lwp_start();
+
+    /* Prints "Hallo thar!" once and exits. */
+    new_lwp(foo2, (void *)212, 256);
     lwp_start();
 
     return 0;
 }
 
 static void foo(void * bar) {
+    GetSP(sp);
+    GetBP(bp);
     printf("Hallo thar!\n");
     lwp_exit();
+}
+
+static void foo2(void * bar) {
+    GetSP(sp);
+    GetBP(bp);
+    printf("Hallo thar!\n");
 }
 
