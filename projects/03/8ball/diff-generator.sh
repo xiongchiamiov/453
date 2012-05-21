9556abb7686221b37240d666b5d736a0a68c4a20
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-05-20 23:27:32 -0700

    try to load up the 8ball driver at boot

--- projects/03/8ball/diff-generator.sh
+++ projects/03/8ball/diff-generator.sh
@@ -7,4 +7,7 @@ diff() {
 diff /usr/src/drivers/Makefile > diffs/drivers_Makefile.diff
 diff /etc/system.conf > diffs/system.conf.diff
 diff /etc/rc > diffs/rc.diff
+diff /usr/src/kernel/table.c > diffs/table.c.diff
+diff /usr/src/include/minix/com.h > diffs/com.h.diff
+diff /usr/src/include/minix/types.h > diffs/types.h
 
